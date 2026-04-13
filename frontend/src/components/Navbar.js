cb7e32f029ab354a821014d7568a8fc694471f82
smarana23 <smarana.vollala@gmail.com>
2026-04-13 11:27:01 +0530

    Hamburger Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,18 +1,32 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
 
-      <h2 className="logo">Hybrid Speech Emotion Recognition System</h2>
+      <h2 className="logo">
+        Hybrid Speech Emotion Recognition System
+      </h2>
 
-      <div className="nav-links">
+      {/* HAMBURGER */}
+      <div 
+        className="hamburger"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        ☰
+      </div>
+
+      {/* LINKS */}
+      <div className={`nav-links ${menuOpen ? "active" : ""}`}>
         <Link to="/">Home</Link>
         <Link to="/record">Record</Link>
         <Link to="/dashboard">Dashboard</Link>
         <Link to="/history">History</Link>
-
       </div>
 
     </nav>
