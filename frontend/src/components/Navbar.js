b383287ec8d7bd822204efeb2ab9a48f6ded0fb7
smarana23 <smarana.vollala@gmail.com>
2026-04-13 11:50:18 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,36 +1,17 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
-
 function Navbar() {
-
-  const [menuOpen, setMenuOpen] = useState(false);
-
   return (
     <nav className="navbar">
-
-      <h2 className="logo">
-        Hybrid Speech Emotion Recognition System
-      </h2>
-
-      {/* HAMBURGER */}
-      <div 
-        className="hamburger"
-        onClick={() => setMenuOpen(!menuOpen)}
-      >
-        ☰
-      </div>
-
-      {/* LINKS */}
-      <div className={`nav-links ${menuOpen ? "active" : ""}`}>
-        <Link to="/">Home</Link>
-        <Link to="/record">Record</Link>
-        <Link to="/dashboard">Dashboard</Link>
-        <Link to="/history">History</Link>
-      </div>
-
+      {" "}
+      <h2 className="logo">Hybrid Speech Emotion Recognition System</h2>{" "}
+      <div className="nav-links">
+        {" "}
+        <Link to="/">Home</Link> <Link to="/record">Record</Link>{" "}
+        <Link to="/dashboard">Dashboard</Link>{" "}
+        <Link to="/history">History</Link>{" "}
+      </div>{" "}
     </nav>
   );
 }
-
 export default Navbar;
